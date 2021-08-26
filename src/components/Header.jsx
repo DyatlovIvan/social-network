edff0bc2be2b8b321fd0c87fd009cfb5040d0be0
Ivan Dyatlov <diatlovivan92@mail.ru>
2021-08-26 23:36:32 +0400

    new

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+const Header = () => {
+    return (<header className='header'>
+        <img src='http://pngimg.com/uploads/telegram/small/telegram_PNG35.png' />
+    </header>)
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+
+const Profile = () => {
+    return ( <div className='content'>
+    <div>
+      <img src='https://vjoy.cc/wp-content/uploads/2019/07/1-1.jpg' />
+    </div>
+
+    <div>
+      <img src='https://klike.net/uploads/posts/2020-04/1587719791_1.jpg' />
+    </div>
+
+    <div>
+      my posts
+      <div>
+        new posts
+        <div>Post1</div>
+        <div>Post2</div>
+      </div>
+
+    </div>
+  </div>)
+}
+
+export default Profile;
